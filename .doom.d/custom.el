93325810ed1d63fd5376fcbbb14ada3c8f2dc467
Tobin Stultiens <tobin.stultiens@gmail.com>
2021-04-22 20:00:01 +0200

    Daily update

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(org-agenda-files '("~/Documents/Org Mode/org-basics.org"))
  '(package-selected-packages '(eglot)))
 (custom-set-faces
