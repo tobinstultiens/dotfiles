65041a911b146aea2c0e7bcf4c122704f549837f
Tobin Stultiens <tobin.stultiens@gmail.com>
2021-07-21 20:00:01 +0200

    Daily update

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -6,7 +6,7 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(org-agenda-files '("~/Documents/Org Mode/org-basics.org"))
- '(package-selected-packages '(eglot)))
+ '(package-selected-packages '(lsp-python-ms eglot)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
