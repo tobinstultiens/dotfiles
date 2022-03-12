7764d19deda87a0614db15af7b89e3ec06ed3503
tobinstultiens <tobinstultiens@gmail.com>
2022-03-12 23:24:04 +0100

    Added devourer

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,9 +6,25 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload example &
+BAR_NAME=example
+BAR_CONFIG=/home/$USER/.config/polybar/config.ini
+
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+# Launch on second monitor
+MONITOR=HDMI-0 polybar --reload $BAR_NAME &
+sleep 1
+
+
+# Launch on all other monitors
+for m in $PRIMARY; do
+ MONITOR=$m polybar --reload $BAR_NAME &
 done
+
+## Launch Polybar, using default config location ~/.config/polybar/config
+#for m in $(polybar --list-monitors | cut -d":" -f1); do
+#    MONITOR=$m polybar --reload example &
+#done
 
 echo "Polybar launched..."
