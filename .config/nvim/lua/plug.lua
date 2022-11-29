6f747809af7f99b534b8e668bfb883962e4c4ab5
tobinstultiens <tobin.stultiens@gmail.com>
2022-11-29 23:06:06 +0100

    Fixed indenting

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -1,49 +1,49 @@
 return require('packer').startup(function()
-    -- other plugins...
-    
-    use 'williamboman/mason.nvim'    
-    use 'williamboman/mason-lspconfig.nvim'
+	-- other plugins...
 
-		-- Rust tools 
-    use 'neovim/nvim-lspconfig' 
-    use 'simrat39/rust-tools.nvim'
+	use 'williamboman/mason.nvim'    
+	use 'williamboman/mason-lspconfig.nvim'
 
-		-- Completion framework:
-    use 'hrsh7th/nvim-cmp' 
+	-- Rust tools 
+	use 'neovim/nvim-lspconfig' 
+	use 'simrat39/rust-tools.nvim'
 
-    -- LSP completion source:
-    use 'hrsh7th/cmp-nvim-lsp'
+	-- Completion framework:
+	use 'hrsh7th/nvim-cmp' 
 
-    -- Useful completion sources:
-    use 'hrsh7th/cmp-nvim-lua'
-    use 'hrsh7th/cmp-nvim-lsp-signature-help'
-    use 'hrsh7th/cmp-vsnip'                             
-    use 'hrsh7th/cmp-path'                              
-    use 'hrsh7th/cmp-buffer'                            
-    use 'hrsh7th/vim-vsnip'
+	-- LSP completion source:
+	use 'hrsh7th/cmp-nvim-lsp'
 
-		-- Treesitter
-    use 'nvim-treesitter/nvim-treesitter'
+	-- Useful completion sources:
+	use 'hrsh7th/cmp-nvim-lua'
+	use 'hrsh7th/cmp-nvim-lsp-signature-help'
+	use 'hrsh7th/cmp-vsnip'                             
+	use 'hrsh7th/cmp-path'                              
+	use 'hrsh7th/cmp-buffer'                            
+	use 'hrsh7th/vim-vsnip'
 
-		-- Vimspector
-    use 'puremourning/vimspector'
+	-- Treesitter
+	use 'nvim-treesitter/nvim-treesitter'
 
-		-- Vim floating terminal
-		use 'voldikss/vim-floaterm'
-   
-		-- Telescope
-		use {
-  		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-			-- or                            , branch = '0.1.x',
-  		requires = { {'nvim-lua/plenary.nvim'} }
-		}
+	-- Vimspector
+	use 'puremourning/vimspector'
 
-		use {
-  		'phaazon/hop.nvim',
-  		branch = 'v2', -- optional but strongly recommended
-  		config = function()
-    		-- you can configure Hop the way you like here; see :h hop-config
-    		require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-  		end
-		}
+	-- Vim floating terminal
+	use 'voldikss/vim-floaterm'
+
+	-- Telescope
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use {
+		'phaazon/hop.nvim',
+		branch = 'v2', -- optional but strongly recommended
+		config = function()
+			-- you can configure Hop the way you like here; see :h hop-config
+			require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+		end
+	}
 end)
