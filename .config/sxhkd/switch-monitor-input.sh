d1167becc5168962e12b312badf68209860c8e0d
tobinstultiens <tobin.stultiens@gmail.com>
2023-02-25 14:40:16 +0100

    Updated

--- .config/sxhkd/switch-monitor-input.sh
+++ .config/sxhkd/switch-monitor-input.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+# Set the display inputs to laptop
+laptopSwitch(){
+	(sudo ddcutil -b 7 setvcp 0x60 0x11)
+	#(sudo ddcutil -b 4 setvcp 0x60 0x11)
+}
+
+# Set the display inputs to pc
+pcSwitch(){
+	(sudo ddcutil -b 7 setvcp 0x60 0x0f)
+	#(sudo ddcutil -b 4 setvcp 0x60 0x12)
+}
+
 # Retrieve information on what input is being used.
-PC=sudo ddcutil -b 7 getvcp 0x60
-LAPTOP=sudo ddcutil -b 4 getvcp 0x60
+PC=$(sudo ddcutil -b 7 getvcp 0x60)
+LAPTOP=$(sudo ddcutil -b 4 getvcp 0x60)
 
 if [[ "$PC" == *"DisplayPort-1"* ]]; then
 	laptopSwitch
@@ -10,14 +22,3 @@ elif [[ "$PC" == *"HDMI-1"* ]]; then
 	pcSwitch
 fi
 
-# Set the display inputs to laptop
-function laptopSwitch{
-sudo ddcutil -b 7 setvcp 0x60 0x11
-sudo ddcutil -b 4 setvcp 0x60 0x11
-}
-
-# Set the display inputs to pc
-function pcSwitch{
-sudo ddcutil -b 7 setvcp 0x60 0x0f
-sudo ddcutil -b 4 setvcp 0x60 0x12
-}
