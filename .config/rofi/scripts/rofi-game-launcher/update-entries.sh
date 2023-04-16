1869d10a84df980fac8ee18fdedf30bfc1f7ee5a
tobinstultiens <tobin.stultiens@gmail.com>
2023-04-16 16:41:10 +0200

    Updated to new version of the open script

--- .config/rofi/scripts/rofi-game-launcher/update-entries.sh
+++ .config/rofi/scripts/rofi-game-launcher/update-entries.sh
@@ -3,20 +3,20 @@
 # Generates .desktop entries for all installed Steam games with box art for
 # the icons to be used with a specifically configured Rofi launcher
 
-SCRIPT_DIR=$(dirname $(realpath $0))
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-STEAM_ROOT=$HOME/.local/share/Steam
-APP_PATH=$HOME/.cache/rofi-game-launcher/applications
+STEAM_ROOT="$HOME/.local/share/Steam"
+APP_PATH="$HOME/.cache/rofi-game-launcher/applications"
 
 # Fetch all Steam library folders.
 steam-libraries() {
     echo "$STEAM_ROOT"
 
     # Additional library folders are recorded in libraryfolders.vdf
-    libraryfolders=$STEAM_ROOT/steamapps/libraryfolders.vdf
+    local libraryfolders="$STEAM_ROOT/steamapps/libraryfolders.vdf"
     # Match directories listed in libraryfolders.vdf (or at least all strings
     # that look like directories)
-    grep -oP "(?<=\")/.*(?=\")" $libraryfolders
+    grep -oP "(?<=\")/.*(?=\")" "$libraryfolders"
 }
 
 # Generate the contents of a .desktop file for a Steam game.
@@ -25,7 +25,7 @@ desktop-entry() {
 cat <<EOF
 [Desktop Entry]
 Name=$2
-Exec=$SCRIPT_DIR/game-splash-menu.sh $1
+Exec=$SCRIPT_DIR/splash-menu.sh $1
 Icon=$3
 Terminal=false
 Type=Application
@@ -59,7 +59,7 @@ update-game-entries() {
         fi
 
         for manifest in "$library"/steamapps/appmanifest_*.acf; do
-            appid=$(basename "$manifest" | tr -dc "[0-9]")
+            appid=$(basename "$manifest" | tr -dc "0-9")
             entry=$APP_PATH/${appid}.desktop
 
             # Don't update existing entries unless doing a full refresh
@@ -72,8 +72,9 @@ update-game-entries() {
             boxart=$STEAM_ROOT/appcache/librarycache/${appid}_library_600x900.jpg
 
             # Search for custom boxart set through the Steam library
-            for boxart_custom in $STEAM_ROOT/userdata/*/config/grid/${appid}p.{png,jpg}; do
-                [ -e $boxart_custom ] && boxart=$boxart_custom
+            boxart_custom_candidates=("$STEAM_ROOT"/userdata/*/config/grid/"${appid}"p.{png,jpg})
+            for boxart_custom in "${boxart_custom_candidates[@]}"; do
+                [ -e "$boxart_custom" ] && boxart="$boxart_custom"
             done
 
             # Filter out non-game entries (e.g. Proton versions or soundtracks) by
@@ -92,4 +93,4 @@ update-game-entries() {
     done
 }
 
-update-game-entries $@
+update-game-entries "$@"
--- .config/rofi/scripts/rofi-options-menu.sh
+++ .config/rofi/scripts/rofi-options-menu.sh
@@ -1,56 +1,65 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$(dirname $(realpath $0))
+# Open a Rofi options menu to lock the screen, logout, suspend, etc.
 
-LOCK=""
-SLEEP=""
-LOGOUT=""
-RESTART=""
-SHUTDOWN=""
+# Populate arrays containing the menu information
+declare -a entries
+declare -A operation icon confirm
+create-menu-entries() {
+    local entry_struct
+    local menu=(
+        # Entry        Icon Operation                   Require confirmation
+        '("Lock"       ""  "swaymsg exec \$lock"       "false")'
+        '("Logout"     "󰍃"  "swaymsg exit"              "true")'
+        '("Suspend"    ""  "systemctl suspend"         "true")'
+        '("Reboot"     ""  "reboot"                    "true")'
+        '("Shutdown"   ""  "shutdown now"              "true")'
+        '("Screenshot" ""  "swaymsg exec \$screenshot" "false")'
+    )
 
-list_icons() {
-    echo $LOCK
-    echo $SLEEP
-    echo $LOGOUT
-    echo $RESTART
-    echo $SHUTDOWN
+    # Create associative arrays out of the fake multidimensional array
+    for entry in "${menu[@]}"; do
+        eval entry_struct="${entry[*]}"
+        name="${entry_struct[0]}"
+        entries+=("$name")
+        icon[$name]="${entry_struct[1]}"
+        operation[$name]="${entry_struct[2]}"
+        confirm[$name]="${entry_struct[3]}"
+    done
 }
 
-handle_option() {
-    case "$1" in
-        "$LOCK")
-            light-locker-command --lock
-            ;;
-        "$SLEEP")
-            if $($SCRIPT_DIR/rofi-confirm.sh "Confirm suspend"); then
-                systemctl suspend
-            fi
-            ;;
-        "$LOGOUT")
-            if $($SCRIPT_DIR/rofi-confirm.sh "Confirm logout"); then
-                i3-msg exit
-            fi
-            ;;
-        "$RESTART")
-            if $($SCRIPT_DIR/rofi-confirm.sh "Confirm reboot"); then
-                reboot
-            fi
-            ;;
-        "$SHUTDOWN")
-            if $($SCRIPT_DIR/rofi-confirm.sh "Confirm shutdown"); then
-                shutdown now
-            fi
-            ;;
-    esac
+# Open the Rofi options menu
+rofi-menu() {
+    for entry in "${entries[@]}"; do
+        echo -e "${icon[$entry]}\t$entry"
+    done | rofi -dmenu -theme options_menu
 }
 
-{
-    SELECTION="$(list_icons | rofi -dmenu -theme options-menu)"
-    handle_option $SELECTION &
-    sleep 0.05
-    ~/.config/conky/conky-wrapper.sh show
-    wait $!
-    ~/.config/conky/conky-wrapper.sh hide
-} &
-sleep 0.05
-~/.config/conky/conky-wrapper.sh show
+# Execute the command corresponding with the selected menu entry
+rofi-select() {
+    local selection="${1##*$'\t'}"
+    [ -z "$selection" ] && exit 1
+    if [ "${confirm[$selection]}" == "true" ]; then
+        rofi-confirm "$selection" && eval "${operation[$selection]}"
+    else
+        eval '${operation[$selection]}'
+    fi
+}
+
+# Open a Rofi menu to prompt for confirmation before executing a command
+rofi-confirm() {
+    local message="Confirm $1?"
+    local options="\t Cancel\n\t Confirm"
+    local selection
+
+    selection="$(echo -e "$options" | \
+        rofi -dmenu -theme options_menu -mesg "$message" )"
+    if [ "${selection##* }" == "Confirm" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+create-menu-entries
+rofi-select "$(rofi-menu)"
