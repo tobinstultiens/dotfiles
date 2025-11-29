af64a487686413ac627640f9b742ca2be6d323de
Tobin Stultiens <tobin.stultiens@gmail.com>
2025-11-30 00:22:28 +0100

    auto commit

--- .scripts/daily-backup-dotfiles.sh
+++ .scripts/daily-backup-dotfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "HOME is: $HOME" >> /home/tobins/git-daily-commit-log
+HOME=/home/tobins
 
-GIT="/usr/bin/git --git-dir=/home/tobins/.cfg --work-tree=/home/tobins"
+GIT="/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME"
 
 $GIT commit -am "auto commit"
 $GIT push
